242a681a637e470e7241e3eb51db1708d40bffe0
Voronwe <voronwe.sobe@gmail.com>
2019-02-25 19:56:07 +0100

    AIController can find the PlayerController

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -6,18 +6,23 @@ void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	auto ControlledTank = GetControlledTank();
-	if (!ControlledTank)
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("AIController not possessing a tank"));
+		UE_LOG(LogTemp, Warning, TEXT("AIController didn't found PlayerTank"));
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("AIController possessing %s"), *ControlledTank->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("AIController found PlayerTank : %s"), *PlayerTank->GetName());
 	}
 }
 
 ATank *ATankAIController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+ATank *ATankAIController::GetPlayerTank() const
+{
+	return Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
 }
